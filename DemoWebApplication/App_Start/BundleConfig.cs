b9eb7997633de209f75721efacdbe11a44c4bb26
TusharKhaire <100033068+TusharKhaire@users.noreply.github.com>
2023-05-19 16:31:14 +0530

    Update BundleConfig.cs

--- DemoWebApplication/App_Start/BundleConfig.cs
+++ DemoWebApplication/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace DemoWebApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+                      // TTTTTTTTTTTT
         }
     }
 }

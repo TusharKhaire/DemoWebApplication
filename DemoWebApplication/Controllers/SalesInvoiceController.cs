7c22b671bfdfe4c5070b44401046cd607df0a905
Tushar Khaire <tusharkhaire099@gmail.com>
2023-07-05 11:49:54 +0530

    Add SalesInvoice As viewmodel and calculate max bill number

---
+++ DemoWebApplication/Controllers/SalesInvoiceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWebApplication.Models;
+
+namespace DemoWebApplication.Controllers
+{
+    public class SalesInvoiceController : Controller
+    {
+        DemoDbEntities dbcon = new DemoDbEntities();
+        // GET: SalesInvoice
+        public ActionResult Index()
+        {
+            return View();
+        }
+        private int BillNumber()
+        {
+            int? billno = 0;
+            billno = dbcon.SalesInvoiceMasters.Max(x => (int?)x.Billno);
+            billno = billno.HasValue ? billno + 1 : 1;
+            return billno??1;
+        }
+        public ActionResult Create()
+        {
+            SalesInvoice sm = new SalesInvoice();
+            sm.Billno = BillNumber();
+            return View(sm);
+        }
+    }
+}
---
+++ DemoWebApplication/Models/SalesInvoice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace DemoWebApplication.Models
+{
+    public class SalesInvoice
+    {
+        //Sales Master Data
+        public int SalesMasterId { get; set; }
+        public int Accountnumber { get; set; }
+        public string Accountname { get; set; }
+        public int Billno { get; set; }
+        public System.DateTime Invoicedate { get; set; }
+        public string PaymentmodeCash { get; set; }
+        public Nullable<int> Totalbillamount { get; set; }
+        public Nullable<int> Paidamount { get; set; }
+        public Nullable<int> Balanceamount { get; set; }
+        public string CAddress { get; set; }
+        public Nullable<long> Phoneno { get; set; }
+        public string Manualno { get; set; }
+        public Nullable<int> BillDiscount { get; set; }
+        public System.DateTime Duedate { get; set; }
+        public bool ApplyGst { get; set; }
+
+        // Sales Details Data
+        public int SalesDetailId { get; set; }
+        //public Nullable<int> Accountnumber { get; set; }
+        //public Nullable<int> Billno { get; set; }
+        public Nullable<int> Itemdetailid { get; set; }
+        public string Batchno { get; set; }
+        public string Godown { get; set; }
+        public Nullable<System.DateTime> Expirydate { get; set; }
+        public Nullable<int> qty { get; set; }
+        public Nullable<int> salesprice { get; set; }
+        public Nullable<int> disc { get; set; }
+        public Nullable<int> discamt { get; set; }
+        public Nullable<int> total { get; set; }
+        public Nullable<int> SGST { get; set; }
+        public Nullable<int> CGST { get; set; }
+        public Nullable<int> IGST { get; set; }
+        public string HSNCode { get; set; }
+        public Nullable<int> SGSTAmt { get; set; }
+        public Nullable<int> SGSTTaxableAmt { get; set; }
+        public Nullable<int> CGSTAmt { get; set; }
+        public Nullable<int> CGSTTaxableAmt { get; set; }
+        public Nullable<int> SrNo { get; set; }
+        public Nullable<int> MRP { get; set; }
+        public Nullable<int> PurchasePrice { get; set; }
+
+
+    }
+}

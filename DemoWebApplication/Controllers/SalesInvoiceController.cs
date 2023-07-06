11c9757f0bae548f14d90355d75241c5a9d58d6e
Tushar Khaire <tusharkhaire099@gmail.com>
2023-07-06 18:31:50 +0530

    Download Customer data-- Item data add to table body pending

--- DemoWebApplication/Controllers/SalesInvoiceController.cs
+++ DemoWebApplication/Controllers/SalesInvoiceController.cs
@@ -62,15 +62,20 @@ namespace DemoWebApplication.Controllers
         public JsonResult GetCustomerName(string searchText)
         {
             var Customer = dbcon.AccountMasters.Where(x => x.AccountName.Contains(searchText)).ToList();
-            return Json(Customer.Select(x=>new {id=x.AccountCode,text=x.AccountName }), JsonRequestBehavior.AllowGet);
+            return Json(Customer.Select(x => new { id = x.AccountCode, text = x.AccountName }), JsonRequestBehavior.AllowGet);
         }
-
+        public JsonResult GetCustomerData(int id)
+        {
+            var Customerdata = dbcon.AccountMasters.Where(x => x.AccountCode == id).FirstOrDefault();
+            return Json(Customerdata, JsonRequestBehavior.AllowGet);
+        }
         public JsonResult GetItemName(string searchText)
         {
             var ItemName = dbcon.ItemMasters.Join(dbcon.ItemDetails, a => a.ItemCode, b => b.ItemMasterId, (a, b) => new { ItemMaster = a, ItemDetail = b }).Select(i => new
-            {   ItemCode=i.ItemDetail.ItemdetailId,
-               ItemName=i.ItemMaster.ItemName,
-               Details= i.ItemDetail.ItemdetailId + ":" + i.ItemMaster.ItemName + ":" + i.ItemDetail.ClosingStock
+            {
+                ItemCode = i.ItemDetail.ItemdetailId,
+                ItemName = i.ItemMaster.ItemName,
+                Details = i.ItemDetail.ItemdetailId + ":" + i.ItemMaster.ItemName + ":" + i.ItemDetail.ClosingStock
             }).Where(x => x.ItemName.Contains(searchText)).ToList();
             return Json(ItemName.Select(x => new { id = x.ItemCode, text = x.Details }), JsonRequestBehavior.AllowGet);
         }
@@ -87,14 +92,16 @@ namespace DemoWebApplication.Controllers
             //public int MfrId;
             public int mrp;
             public int purchaseprice;
-            public DateTime expdate;
-            public double closingstock ;
+            public string expdate;
+            public double closingstock;
+            public string HSNCOde;
+            public double Gst;
         }
         public JsonResult GetItemByName(string searchText)
-      {
+        {
             List<itemdata> lst_Itemdata = new List<itemdata>();
-            var ItemData = dbcon.ItemMasters.Join(dbcon.ItemDetails, a => a.ItemCode, b => b.ItemMasterId, (a, b) => new { ItemMaster = a, ItemDetail = b }).Where(x=>x.ItemMaster.ItemName.Contains(searchText)).ToList();
-            foreach(var item in ItemData)
+            var ItemData = dbcon.ItemMasters.Join(dbcon.ItemDetails, a => a.ItemCode, b => b.ItemMasterId, (a, b) => new { ItemMaster = a, ItemDetail = b }).Where(x => x.ItemMaster.ItemName.Contains(searchText)).ToList();
+            foreach (var item in ItemData)
             {
                 itemdata obj = new itemdata();
                 var batch = dbcon.BatchMasters.Where(x => x.BatchId == item.ItemDetail.BatchId).FirstOrDefault();
@@ -106,15 +113,37 @@ namespace DemoWebApplication.Controllers
                 obj.ItemType = Type.TypeName;
                 obj.Batch = batch.BatchName;
                 obj.Godown = Godown.GodownName;
-                obj.mrp = item.ItemDetail.MRP??0;
-                obj.purchaseprice = item.ItemDetail.PurchasePrice??0;
-                obj.closingstock = item.ItemDetail.ClosingStock??0;
+                obj.mrp = item.ItemDetail.MRP ?? 0;
+                obj.purchaseprice = item.ItemDetail.PurchasePrice ?? 0;
+                obj.closingstock = item.ItemDetail.ClosingStock ?? 0;
                 lst_Itemdata.Add(obj);
             }
 
-            return Json(lst_Itemdata.Select(x=>new {id=x.Itemdetailid,text=x.Itemname + ":" + x.ItemType + ":" + x.Batch + ":" + x.Godown + ":" + x.closingstock }),JsonRequestBehavior.AllowGet);  // 
+            return Json(lst_Itemdata.Select(x => new { id = x.Itemdetailid, text = x.Itemname + ":" + x.ItemType + ":" + x.Batch + ":" + x.Godown + ":" + x.closingstock }), JsonRequestBehavior.AllowGet);  // 
         }
+        public JsonResult GetItemsData(int code)
+        {
+            var item = dbcon.ItemMasters.Join(dbcon.ItemDetails, a => a.ItemCode, b => b.ItemMasterId, (a, b) => new { ItemMaster = a, ItemDetail = b }).Where(x => x.ItemDetail.ItemdetailId == code).FirstOrDefault();
 
+            itemdata obj = new itemdata();
+            var batch = dbcon.BatchMasters.Where(x => x.BatchId == item.ItemDetail.BatchId).FirstOrDefault();
+            var Godown = dbcon.GodownMasters.Where(x => x.GodownId == item.ItemDetail.GodownId).FirstOrDefault();
+            var Type = dbcon.ItemTypes.Where(x => x.TypeId == item.ItemMaster.ItemType).FirstOrDefault();
+            obj.Itemdetailid = item.ItemDetail.ItemdetailId;
+            obj.Itemmasterid = item.ItemMaster.ItemCode;
+            obj.Itemname = item.ItemMaster.ItemName;
+            obj.ItemType = Type.TypeName;
+            obj.Batch = batch.BatchName;
+            obj.Godown = Godown.GodownName;
+            obj.mrp = item.ItemDetail.MRP ?? 0;
+            obj.purchaseprice = item.ItemDetail.PurchasePrice ?? 0;
+            obj.closingstock = item.ItemDetail.ClosingStock ?? 0;
+            obj.HSNCOde = item.ItemMaster.HSNCODE;
+            obj.Gst =Convert.ToDouble( item.ItemMaster.GstPer);
+           
+            //obj.expdate = item.ItemDetail.Expirydate?? 0;   
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
 
     }
 }
